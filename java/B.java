59be6f59a90f50608c953fab6e2cb6c4debefa90
skyofsmith <skyofsmith@163.com>
2018-06-19 07:58:31 -0700

    java

--- java/B.java
+++
@@ -1,9 +0,0 @@
-/**
- * B
- */
-public class B {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- javaDemo/src/javaDemo/Demo.java
+++ javaDemo/src/javaDemo/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
 	public static void main (String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("I am Sam");
+                //i am not yet forgot the "hello world"!!!
 	}
 }
