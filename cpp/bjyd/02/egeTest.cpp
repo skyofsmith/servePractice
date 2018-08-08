bd4d28435b7ef1f8583f7aff60a6dd7777008e54
drzz <skyofsmith@163.com>
2018-08-08 23:45:49 +0800

    ege graph aloha world!

---
+++ cpp/bjyd/02/egeTest.cpp
@@ -0,0 +1,13 @@
+#include "graphics.h"
+
+int main() {
+    initgraph(320, 240);
+	
+	outtextxy(20, 120, "Aloha World!");
+	line(10, 10, 300, 200);
+	
+	ege::getch();
+	closegraph();
+	
+	return 0;
+}
