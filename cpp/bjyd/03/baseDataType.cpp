2f13d3bc1b117c64457622b0e6690a4b94587440
drzz <skyofsmith@163.com>
2018-08-10 23:19:44 +0800

    baseDataType

---
+++ cpp/bjyd/03/baseDataType.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main () {
+    int x = 1;
+    int y(2);
+    int z;
+    //z(3); //error!
+    
+    std::cout << "  x=" << x <<
+                 "\ty=" << y << std::endl;
+    std::cin.get();
+    return 0;
+}
