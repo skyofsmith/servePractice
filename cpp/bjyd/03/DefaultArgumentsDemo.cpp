21dc27e50c69a437bd9ee8f018a87bd1d4202839
drzz <skyofsmith@163.com>
2018-08-13 20:47:15 +0800

    DefaultArgumentsDemo++

--- cpp/bjyd/03/DefaultArgumentsDemo.cpp
+++ cpp/bjyd/03/DefaultArgumentsDemo.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void printArea (double radius = 1) {
-    double area = radius * radius * 3.14159;
-    cout << "area is " << area << endl;
-}
+void printArea (double radius = 1);
+//void printArea (double radius = 1) {
+//    double area = radius * radius * 3.14159;
+//    cout << "area is " << area << endl;
+//}
 
 int main(int argc, char** argv) {
     printArea();
     printArea(4);
 	return 0;
+}
+
+void printArea (double radius) {
+    double area = radius * radius * 3.14159;
+    cout << "area is " << area << endl;
 }
