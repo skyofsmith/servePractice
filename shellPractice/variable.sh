f8b0c75a9c32433bdd23231268666db620ce7d04
drzz <skyofsmith@163.com>
2018-08-26 22:50:28 +0800

    variable.sh

--- shellPractice/variable.sh
+++ shellPractice/variable.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+#使用变量
 your_name=zz
 echo $your_name
 echo ${your_name}
 
+#只读变量
 myUrl="http://www.google.com"
 readonly myUrl
 #myUrl="http://www.runoob.com"#报错
 
+#删除变量
 canRemove = 123
 unset canRemove
 echo 'canRemove is ' $canRemove
+
+#字符串
+
