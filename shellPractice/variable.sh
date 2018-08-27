d2efc5d7dddbdb018a8e4d538df1c8a5a4f92acd
drzz <skyofsmith@163.com>
2018-08-27 21:21:32 +0800

    variable.sh

--- shellPractice/variable.sh
+++ shellPractice/variable.sh
@@ -16,7 +16,24 @@ unset canRemove
 echo 'canRemove is ' $canRemove
 
 #字符串
+#单引号
 str='this is a string'
 echo str
+#双引号
 str="Hello, I know you are \"$your_name\"! \n"
 echo str
+
+# 使用双引号拼接
+your_name="runoob"
+greeting="hello, "$your_name" !"
+greeting_1="hello, ${your_name} !"
+echo $greeting  $greeting_1
+
+# 使用单引号拼接
+greeting_2='hello, '$your_name' !'
+greeting_3='hello, ${your_name} !'
+echo $greeting_2  $greeting_3
+
+# 获取字符串长度
+string="abcd"
+echo ${#string} #输出 4
