ceeb5bf14dd50aedefad6689be4b9d3392ffbb5c
drzz <skyofsmith@163.com>
2018-09-02 22:27:30 +0800

    echo.sh

---
+++ shellPractice/echo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "it is a string!"
+
+echo it is a string!
+
+echo "\"It is a test\""
+
+read name
+echo "$name It is a test"
+
+echo -e "OK! \n"
+echo "it is a string!"
+
+echo -e "OK! \c"
+echo "it is a string!"
+
+echo "aaa" > tempFile
+
+echo '$name'
+
+echo `date`
+
