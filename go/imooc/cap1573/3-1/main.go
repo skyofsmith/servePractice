2de19843cc1294b64ee2fc2ff9dfb6f4a5b584bd
drzz <skyofsmith@163.com>
2018-09-19 23:39:33 +0800

    add comments to 3-1

--- go/imooc/cap1573/3-1/main.go
+++ go/imooc/cap1573/3-1/main.go
@@ -6,8 +6,22 @@ const NAME = "imooc"
 
 var mainName = "main name"
 
+/*
+ 多行
+ 注释
+*/
 func main() {
+	// 单行注释
 	fmt.Println("hello world")
 	fmt.Print(mainName)
 	fmt.Print(NAME)
 }
+
+/*
+关键字:
+break		default		func	interface	select
+case		defer		go		map			struct
+chan		else		goto	package		switch
+const		fallthrough	if		range		type
+continue	for			import	return		var
+*/
