6fa076dbbf16a76b1f4d1918597553a246270ac4
drzz <skyofsmith@163.com>
2018-09-20 23:36:25 +0800

    3-4

---
+++ go/imooc/cap1573/3-4/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	//	"time"	//error, not used
+)
+
+func main() {
+	fmt.Println("Hello world")
+}
+
+/*
+method 1:
+import "fmt"
+import "time"
+
+method 2:
+import (
+	"fmt"
+	"time"
+)
+
+*/
