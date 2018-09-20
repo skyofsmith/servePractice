93745112166f3472c45e1e5e8b56b244fc670362
drzz <skyofsmith@163.com>
2018-09-20 23:59:08 +0800

    TestFriendClass

---
+++ cpp/bjyd/05/TestFriendFunction.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Date
+{
+    friend void p();
+private:
+    int year;
+    int month;
+    int day;
+};
+
+void p()
+{
+    Date date;
+    date.year = 2000;
+    cout << date.year;
+}
+
+int main() {
+    p();
+	return 0;
+}
