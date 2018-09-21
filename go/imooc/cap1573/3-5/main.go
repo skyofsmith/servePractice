83ffc8644a02903c4a770fd6140d474afc95e25c
drzz <skyofsmith@163.com>
2018-09-21 23:02:08 +0800

    3-5 comments

--- go/imooc/cap1573/3-5/main.go
+++ go/imooc/cap1573/3-5/main.go
@@ -7,8 +7,32 @@ import (
 	"./show2"
 )
 
+func init() {
+	fmt.Println("main init")
+}
+
 func main() {
 	learn1.Learn1()
 	show2.Show2()
 	fmt.Print("Hello imooc")
 }
+
+/*
+main
+import pkg1
+			pkg1
+			import pkg2
+							pkg2
+							const ...
+							var ...
+							init()
+			const ...
+			var ...
+			init()
+
+const ...
+var ...
+init()
+main()
+
+*/
