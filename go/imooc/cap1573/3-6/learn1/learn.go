a611e0620b5edb2c957ad398939354ebef396dc8
drzz <skyofsmith@163.com>
2018-09-21 23:21:03 +0800

    3-6

---
+++ go/imooc/cap1573/3-6/learn1/learn.go
@@ -0,0 +1,13 @@
+package learn1
+
+import (
+	. "fmt"
+)
+
+func init() {
+	Println("hello learn1")
+}
+
+func Learn1() {
+
+}
---
+++ go/imooc/cap1573/3-6/show2/show.go
@@ -0,0 +1,15 @@
+package show2
+
+import (
+	"fmt"
+
+	"../learn1"
+)
+
+func init() {
+	fmt.Println("hello show2")
+}
+
+func Show2() {
+	learn1.Learn1()
+}
