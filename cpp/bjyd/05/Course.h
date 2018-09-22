68b3df5461625cf5e2c1bfd206a4ffbdae46ef2f
drzz <skyofsmith@163.com>
2018-09-22 23:37:08 +0800

    TestCourse another time

---
+++ cpp/bjyd/05/Course.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+using namespace std;
+class Course
+{
+public:
+    Course(const string &name);
+    string getName();
+    void  addStudent(const string &student);
+    string * getStudents();
+    int getNumberOfStudents();
+private:
+    string name;
+    string students[100];
+    int numberOfStudents;
+};
