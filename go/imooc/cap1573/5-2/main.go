2b8ac0255f5444bb8abc92a945abdf87f1b4757b
drzz <skyofsmith@163.com>
2018-09-24 18:30:32 +0800

    5-2

---
+++ go/imooc/cap1573/5-2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 2
+	b := 2
+	fmt.Println("a: ", a, "b: ", b)
+
+	fmt.Println("a==b", a == b)
+	fmt.Println("a!=b", a != b)
+	fmt.Println("a>b ", a > b)
+	fmt.Println("a<b ", a < b)
+	
+	fmt.Println("a>=b ", a >= b)
+	fmt.Println("a<=b ", a <= b)
+
+}
