a0b930f234b987eeafc09283d1a08823abea1cb5
skyofsmith <skyofsmith@163.com>
2018-09-25 23:59:48 +0800

    6-2

---
+++ go/imooc/cap1573/6-2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 3
+	switch a {
+	case 1:
+		fmt.Println("=1")
+	case 2:
+		fmt.Println("=2")
+	default:
+		fmt.Println("neither")
+	}
+
+	var b interface{}
+	b = 2
+	switch b.(type) {
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
+	}
+}
