18ff7ae30af5e5c8707af72cc7ca059bf4625699
drzz <skyofsmith@163.com>
2018-10-01 18:03:55 +0800

    add request param

--- go/imooc/ccmouse/helloworldserver/server.go
+++ go/imooc/ccmouse/helloworldserver/server.go
@@ -9,8 +9,10 @@ func main() {
     http.HandleFunc("/", func(
         writer http.ResponseWriter,
         request *http.Request)  {
-          fmt.Fprintln(writer,
-            "<h1>Hello world!</h1>")
+          fmt.Fprintf(writer,
+            "<h1>Hello world %s!</h1>",
+            request.FormValue("name"))
+          
     })
 
     http.ListenAndServe(":8888", nil)
