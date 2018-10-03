3f56723c304554263f38a9e6d381a70f590d881a
drzz <skyofsmith@163.com>
2018-10-03 12:16:01 +0800

    use random create test file

--- go/imooc/ccmouse/pipeline/main.go
+++ go/imooc/ccmouse/pipeline/main.go
@@ -1,26 +1,63 @@
 package main
 
 import (
-  "./pipeline"
-  "fmt"
+	"bufio"
+	"fmt"
+	"os"
+
+	"./pipeline"
 )
 
 func main() {
-  p := pipeline.Merge(
-    pipeline.InMemSort(
-      pipeline.ArraySourcce(3, 2, 6, 7, 4)),
-    pipeline.InMemSort(
-      pipeline.ArraySourcce(7, 4, 0, 3, 2, 13, 8)))
-  for v := range p {
-    fmt.Println(v)
-  }
-  /*
-  for {
-    if num, ok := <- p; ok {
-      fmt.Println(num)
-    } else {
-      break
-    }
-  }
-  */
+	const filename = "large.in"
+	const n = 100000000
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p := pipeline.RandomSource(n)
+
+  writer := bufio.NewWriter(file)
+	pipeline.WriterSink(writer, p)
+  writer.Flush()
+
+	file, err = os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p = pipeline.ReaderSource(bufio.NewReader(file))
+	count := 0
+	for v := range p {
+		fmt.Println(v)
+		count++
+		if count >= 100 {
+			break
+		}
+	}
+
+	// NergeDemo()
+	/*
+	  for {
+	    if num, ok := <- p; ok {
+	      fmt.Println(num)
+	    } else {
+	      break
+	    }
+	  }
+	*/
+}
+
+func MergeDemo() {
+	p := pipeline.Merge(
+		pipeline.InMemSort(
+			pipeline.ArraySourcce(3, 2, 6, 7, 4)),
+		pipeline.InMemSort(
+			pipeline.ArraySourcce(7, 4, 0, 3, 2, 13, 8)))
+	for v := range p {
+		fmt.Println(v)
+	}
 }
