aac28a28a5e80db5b007b6188a3ed252688d2bcd
drzz <skyofsmith@163.com>
2018-10-10 22:59:19 +0800

    baseDataType.go

---
+++ go/runoob/baseDataType/baseDataType.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  var boolValue bool = true;
+  fmt.Println("boolValue", boolValue)
+  var intValue int = 123
+  var floatValue float32 = 1.2
+  fmt.Println("intValue", intValue)
+  fmt.Println("floatValue", floatValue)
+  var stringValue string = "abc"
+  fmt.Println("stringValue", stringValue)
+  
+}
