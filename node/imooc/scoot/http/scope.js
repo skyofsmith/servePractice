3d187b10c3ae44b69c8a6859bcadc62dd299fea9
drzz <skyofsmith@163.com>
2018-12-01 21:18:40 +0800

    scope.js

--- node/imooc/scoot/http/scope.js
+++ node/imooc/scoot/http/scope.js
@@ -1,1 +1,10 @@
 var globalVariable = 'This is global variable';
+
+function globalFunction() {
+    var localVariable = 'This is global variable';
+    console.log('Visit global/local variable');
+    console.log(globalVariable);
+    console.log(localVariable);
+}
+
+globalFunction();
