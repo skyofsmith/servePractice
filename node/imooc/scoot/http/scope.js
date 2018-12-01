de2c259f7df69fbeae230a462c33316cf400a1cc
drzz <skyofsmith@163.com>
2018-12-01 21:47:22 +0800

    scope.js

--- node/imooc/scoot/http/scope.js
+++ node/imooc/scoot/http/scope.js
@@ -5,6 +5,21 @@ function globalFunction() {
     console.log('Visit global/local variable');
     console.log(globalVariable);
     console.log(localVariable);
+
+    globalVariable = 'This is changed variable';
+
+    console.log(globalVariable);
+
+    function localFunction() {
+        var innerLocalVariable = 'This is inner local variable';
+
+        console.log('Visit global/local/innerLocal variable');
+        console.log(globalVariable);
+        console.log(localVariable);
+        console.log(innerLocalVariable);
+    }
+
+    localFunction();
 }
 
 globalFunction();
