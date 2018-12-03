31f843821f32998e88dad7fe44a9239409485027
drzz <skyofsmith@163.com>
2018-12-03 22:29:28 +0800

    comment.js

---
+++ node/imooc/scoot/events/comment.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var querystring = require('querystring');
+
+var postData = querystring.stringify({
+    'content': 'hahaha',
+    'cid': 123
+});
+
+var options = {
+    hostname: 'www.imooc.com',
+    port: 80,
+    path: '/course/document',
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json, text/javascript'
+    }
+};
+
+var req = http.request(options, function(res) {
+    console.log('Status: ' + res.statusCode);
+    console.log('headers: ' + JSON.stringify(res.headers));
+
+    res.on('data', function(chunk) {
+        console.log(Buffer.isBuffer(chunk));
+        console.log(typeof chunk);
+    });
+
+    res.on('end', function(chunk) {
+        console.log('comment finished!');
+    });
+
+});
