70a3fc40f37a1681b5e9617c2d325c698c09c989
drzz <skyofsmith@163.com>
2019-01-16 23:01:15 +0800

    express learn

--- node/express/org/2/routes/index.js
+++ node/express/org/2/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', person: 'nobody' });
+});
+router.get('/a', function(req, res, next) {
+  res.render('index', { title: 'Express', person: 'person a' });
 });
 
 module.exports = router;
