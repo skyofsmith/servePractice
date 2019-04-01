7df2b43c1a4aa41ed53e6fa824b278d6dffdf5e1
drzz <skyofsmith@163.com>
2019-04-01 23:50:19 +0800

    learn rust

---
+++ rust/demo0/target/debug/deps/demo0-a76646f9ab43377a.d
@@ -0,0 +1,5 @@
+G:\GIT\servePractice\rust\demo0\target\debug\deps\demo0-a76646f9ab43377a.exe: src\main.rs
+
+G:\GIT\servePractice\rust\demo0\target\debug\deps\demo0-a76646f9ab43377a.d: src\main.rs
+
+src\main.rs:
