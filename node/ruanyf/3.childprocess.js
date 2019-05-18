1002d3ce1025318d6216e48e83d71d17eee4aab7
drzz <skyofsmith@163.com>
2019-05-18 22:05:02 +0800

    node: child_process

--- node/ruanyf/3.childprocess.js
+++ node/ruanyf/3.childprocess.js
@@ -1,5 +1,8 @@
 const childProcess = require('child_process');
+const path = require('path');
 const exec = childProcess.exec;
+const execSync = childProcess.execSync;
+const execFile = childProcess.execFile;
 
 // 1.exec
 {
@@ -31,3 +34,40 @@ const exec = childProcess.exec;
   })
 }
 
+// 2.execSync
+{
+  let SEPARATOR = process.platform === 'win32' ? ';' : ':';
+  let env = Object.assign({}, process.env);
+
+  env.PATH = path.resolve('./node_modules/.bin') + SEPARATOR + env.PATH;
+
+  function myExecSync(cmd) {
+    console.log('process.cwd() is ', process.cwd());
+    console.log('__dirname is ', __dirname);
+    console.log('process.cwd() === __dirname is ', process.cwd() === __dirname);
+    let output = execSync(cmd, {
+      cwd: process.cwd(),
+      env: env
+    });
+
+    console.log(output);
+  }
+
+  myExecSync('eslint --version');
+}
+
+// 3.execFile
+{
+  let path = '.';
+  execFile('/bin/ls', ['-l', path], (err, result) => {
+    if (err) {
+      console.log(err)
+    }
+    console.log(result)
+  })
+}
+
+// 4.spawn
+{
+
+}
