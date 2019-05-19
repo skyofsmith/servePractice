9c3318900ce33c841770062491c141f06de3afea
drzz <skyofsmith@163.com>
2019-05-19 23:09:26 +0800

    node: cluster

--- node/ruanyf/4.cluster.js
+++ node/ruanyf/4.cluster.js
@@ -3,6 +3,7 @@ const os = require('os');
 const http = require('http');
 
 {
+  /*
   if (cluster.isMaster) {
     for (let i = 0, n = os.cpus().length; i < n; i += 1) {
       cluster.fork();
@@ -11,6 +12,31 @@ const http = require('http');
     http.createServer(function (req, res) {
       res.writeHead(200);
       res.end("hello world\n");
-    }).listen(8000);
+    }).listen(8000, () => {
+      console.log('cluster.isMaster is ', cluster.isMaster)
+    });
+  }
+  */
+}
+
+{
+  console.log('begin run');
+  if(cluster.isMaster) {
+    let numWorkers = os.cpus().length;
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+    for(let i = 0; i < numWorkers; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('online', function(worker) {
+      console.log('Worker ' + worker.process.pid + ' is online');
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+      console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+      console.log('Starting a new worker');
+      cluster.fork();
+    });
   }
 }
