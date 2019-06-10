ce66e995fed7dcd03576bf6d52cbe2ba5c6ea97c
drzz <skyofsmith@163.com>
2019-06-10 23:04:44 +0800

    deal with await async.

--- node/postgresql/index.js
+++ node/postgresql/index.js
@@ -1,33 +1,23 @@
-const pg = require('pg');
+require("babel-core/register");
+const {Client} = require('pg');
 const conString = "postgres://postgres:root@localhost/test";
 
 //this initializes a connection pool
 //it will keep idle connections open for a (configurable) 30 seconds
 //and set a limit of 20 (also configurable)
-let client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('select * from public.user', function(err, result) {
-    //call `done()` to release the client back to the pool
-    // pg.end();
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.debug(result.rows);
-    let len = result.rows.length + 1
-    client.query(`insert into public.user(id, name, age) values(${len}, '${len}th', 28)`, (err, result) => {
-      if(err) {
-        return console.error('error running query', err);
-      }
-      console.debug(result);
-      client.query(`update public.user set age = 29 where id = 1`).then((err, result) => {
-        if(err) {
-          return console.error('error running query', err);
-        }
-        console.debug(result);
-      })
-    });
-  });
-});
+async function main() {
+  let client = new Client(conString);
+  await client.connect();
+  const {rows: qRes} = await client.query('select * from public.user order by id');
+  console.debug(qRes.rows);
+  let len = qRes.length + 1;
+  let iRes = await client.query(`insert into public.user(id, name, age) values(${len}, '${len}th', 28)`);
+  console.log(iRes);
+  let uRes = await client.query(`update public.user set age = 29 where id = ${len}`);
+  console.log(uRes);
+  let dRes = await client.query(`delete from public.user where id = ${len}`);
+  console.log(dRes);
+  await client.end();
+}
+
+main();
