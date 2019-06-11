353fcaf1379c0c3397eaca3b2850c7e11f5fd95a
drzz <skyofsmith@163.com>
2019-06-11 21:17:35 +0800

    change sql

--- node/postgresql/index.js
+++ node/postgresql/index.js
@@ -8,14 +8,14 @@ const conString = "postgres://postgres:root@localhost/test";
 async function main() {
   let client = new Client(conString);
   await client.connect();
-  const {rows: qRes} = await client.query('select * from public.user order by id');
+  const {rows: qRes} = await client.query('select * from "user" order by id');
   console.debug(qRes.rows);
   let len = qRes.length + 1;
-  let iRes = await client.query(`insert into public.user(id, name, age) values(${len}, '${len}th', 28)`);
+  let iRes = await client.query(`insert into "user"(id, name, age) values(${len}, '${len}th', 28)`);
   console.log(iRes);
-  let uRes = await client.query(`update public.user set age = 29 where id = ${len}`);
+  let uRes = await client.query(`update "user" set age = 29 where id = ${len}`);
   console.log(uRes);
-  let dRes = await client.query(`delete from public.user where id = ${len}`);
+  let dRes = await client.query(`delete from "user" where id = ${len}`);
   console.log(dRes);
   await client.end();
 }
