9cc04e1cd8685daa215cea79b24378386333cae0
skyofsmith <skyofsmith@163.com>
2019-06-22 20:50:27 +0800

    mysql delete data use async. mysql update 30000 data use async.

--- node/mysql/addCount.js
+++ node/mysql/addCount.js
@@ -16,14 +16,19 @@ function createRandom() {
 }
 
 async function main() {
+
+  console.time('delete');
+  let deleteRes = await deleteAll();
+  console.log(deleteRes);
+  console.timeEnd('delete');
+
   console.time('queryCount');
   let countRes = await query('select count(*) as count from count');
   console.log(countRes[0].count);
   console.timeEnd('queryCount');
 
   console.time('query');
-  let queryRes = await query('select * from count');
-  console.log(queryRes);
+  await query('select * from count');
   console.timeEnd('query');
 
   console.time('insert');
@@ -33,11 +38,19 @@ async function main() {
   }
   console.timeEnd('insert');
 
+  console.time('query');
+  await query('select * from count');
+  console.timeEnd('query');
 
   console.time('query');
   let queryRes2 = await query('select * from count');
   console.log(queryRes2);
   console.timeEnd('query');
+
+  console.time('update');
+  let updateRes = await updateAll();
+  console.log(updateRes);
+  console.timeEnd('update');
 
   await connection.end();
 }
@@ -54,6 +67,26 @@ async function query (sql, arr) {
 async function insertRow(id, count) {
   return new Promise((resolve, reject) => {
     connection.query(`insert into count values(?, ?)`, [id, count], (err, res) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(res)
+    })
+  })
+}
+async function updateAll() {
+  return new Promise((resolve, reject) => {
+    connection.query(`update count set count = count + 1`, (err, res) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(res)
+    })
+  })
+}
+async function deleteAll() {
+  return new Promise((resolve, reject) => {
+    connection.query(`delete from count`, (err, res) => {
       if (err) {
         reject(err)
       }
