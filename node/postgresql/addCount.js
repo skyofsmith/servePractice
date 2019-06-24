6710188ccd0ccbdc44729c634c3c3b0eedfc7b45
skyofsmith <skyofsmith@163.com>
2019-06-24 23:35:15 +0800

    change pg to count time.

--- node/postgresql/addCount.js
+++ node/postgresql/addCount.js
@@ -12,14 +12,30 @@ function createRandom () {
 async function main() {
   let client = new Client(conString);
   await client.connect();
-  console.time('insert');
+
+  console.time('delete');
+  await client.query('delete from count');
+  console.timeEnd('delete');
+
+  console.time('query');
+  await client.query('select * from count');
+  console.timeEnd('query');
+
+  console.time('queryCount');
   const {rows: qRes} = await client.query('select count(*) from count');
+  console.timeEnd('queryCount');
+
   let count = qRes[0].count;
-  console.debug(count);
+  console.time('insert');
   for (let i = 0; i < NUMBER_COUNTS; i++) {
     await client.query(`insert into count(id, count) values($1, $2)`, [count++, createRandom()]);
   }
   console.timeEnd('insert');
+
+  console.time('update');
+  await client.query('update count set count = count + 1');
+  console.timeEnd('update');
+
   await client.end();
 }
 
