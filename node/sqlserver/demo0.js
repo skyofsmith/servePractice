367e4966b9bd751b612c081f6520ffda0c5328d9
skyofsmith <skyofsmith@163.com>
2019-06-28 22:35:34 +0800

    mssql try.

--- node/sqlserver/demo0.js
+++ node/sqlserver/demo0.js
@@ -1,5 +1,5 @@
 const sql = require('mssql');
-const db = {};
+
 const config = {
   user: 'sa',
   password: 'root',
@@ -7,18 +7,15 @@ const config = {
   database: 'test'
 };
 
-// async () => {
-//   try {
-//     await sql.connect('mssql://sa:root@localhost/test')
-//     // select * from user
-//     const result = await sql.query(`SELECT TOP (200) id, name, age FROM [user]`)
-//     console.dir(result)
-//   } catch (err) {
-//     console.log(err)
-//   }
-// }
+sql.connect(config).then(() => {
+  return sql.query`select * from [user]`
+}).then(result => {
+  console.dir(result);
 
-sql.connect('mssql://sa:root@localhost/test', res => {
-  console.log(res)
-  console.log(arguments)
-})
+}).catch(err => {
+  console.error(err);
+});
+
+sql.on('error', err => {
+  console.error(err);
+});
