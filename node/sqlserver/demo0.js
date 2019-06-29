05a4f2e6c97aa044d1c5eb2f0a199ca1b9d19f15
skyofsmith <skyofsmith@163.com>
2019-06-29 20:34:04 +0800

    mssql connect fail problem resolved.

--- node/sqlserver/demo0.js
+++ node/sqlserver/demo0.js
@@ -4,13 +4,16 @@ const config = {
   user: 'sa',
   password: 'root',
   server: 'localhost',
-  database: 'test'
+  database: 'test',
+  options: {
+    encrypt: true //使用windows azure，需要设置次配置。
+  }
 };
 
 sql.connect(config).then(() => {
   return sql.query`select * from [user]`
 }).then(result => {
-  console.dir(result);
+  console.log(result);
 
 }).catch(err => {
   console.error(err);
