9d948d948f8ed30a8dca338018a51b77a9c1321e
skyofsmith <skyofsmith@163.com>
2019-07-09 21:22:42 +0800

    electron

--- electron/electron-quick-start/renderer.js
+++ electron/electron-quick-start/renderer.js
@@ -1,3 +1,9 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+console.log('window:', window);
+setInterval(() => {
+  let time = document.querySelector('#time');
+  let now = new Date();
+  time.innerHTML = `${now.getFullYear()}年${now.getMonth() + 1}月${now.getDate()}日`;
+}, 500);
