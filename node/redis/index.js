33e942ff63c012b45adee73bf079a7bd259576dd
skyofsmith <skyofsmith@163.com>
2019-08-26 21:05:13 +0800

    redis error

--- node/redis/index.js
+++ node/redis/index.js
@@ -6,6 +6,7 @@ const client = redis.createClient('6379', '127.0.0.1');
 
 client.on("error", function (err) {
   console.log("Error " + err);
+  client.quit();
 });
 
 client.set("name", "sam", redis.print);
