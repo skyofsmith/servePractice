0a0e35f9514e84ea9ee42ebd55d260542b4a89ea
skyofsmith <skyofsmith@163.com>
2019-09-25 23:15:29 +0800

    important word

--- go/netease/go-base-program/2/baseType.go
+++ go/netease/go-base-program/2/baseType.go
@@ -7,3 +7,15 @@ import (
 func main()  {
 	
 }
+/*
+Go内置关键字（25个均为小写）
+break	default		func	interface	select
+case	defer		go		map			struct
+chan	else		goto	package		switch
+const	fallthrough	if		range		type
+continue	for		import	return		var
+*/
+
+//Go注释方法
+//单行注释
+/* 多行注释 */
