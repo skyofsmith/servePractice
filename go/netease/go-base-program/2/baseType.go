2427d8d7eab73721742b8b3806fe6dbd20651c17
skyofsmith <skyofsmith@163.com>
2019-09-27 00:05:05 +0800

    add comment

--- go/netease/go-base-program/2/baseType.go
+++ go/netease/go-base-program/2/baseType.go
@@ -1,11 +1,29 @@
+// 当前程序的包名
 package main
 
+// 导入其他的包
 import (
 	"fmt"
 )
 
+// 常量的定义
+const PI = 3.14
+
+// 全局变量
+var name = "gopher"
+
+// 一般类型声明
+type newType int
+
+// 结构的声明
+type gopher struct {}
+
+// 接口的声明
+type golang interface {}
+
+// 由main函数作为程序入口启动点
 func main()  {
-	
+	fmt.Println("Hello world!")
 }
 /*
 Go内置关键字（25个均为小写）
