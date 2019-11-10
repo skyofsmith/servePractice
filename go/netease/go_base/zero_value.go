a6f42bc0ef38fb24ed7d0fda10c6aa2631508c54
skyofsmith <skyofsmith@163.com>
2019-11-10 21:08:41 +0800

    multi variable assignment

--- go/netease/go_base/zero_value.go
+++ go/netease/go_base/zero_value.go
@@ -10,6 +10,39 @@ type (
 
 
 func main () {
+    test()
+    var (
+        aaa = "hello"
+        sss, bbb = 1, 2
+    )
+    fmt.Println(aaa)
+    fmt.Println(sss)
+    fmt.Println(bbb)
+
+    var a, b, c, d int
+    a, b, c, d = 1, 2, 3, 4
+    fmt.Println(a)
+    fmt.Println(b)
+    fmt.Println(c)
+    fmt.Println(d)
+
+    var e, f, g, h int = 5, 6, 7, 8
+    fmt.Println(e)
+    fmt.Println(f)
+    fmt.Println(g)
+    fmt.Println(h)
+    var i, j, k, l = 9, 10, 11, 12
+    fmt.Println(i)
+    fmt.Println(j)
+    fmt.Println(k)
+    fmt.Println(l)
+    m, n, o := 13, 14, 15
+    fmt.Println(m)
+    fmt.Println(n)
+    fmt.Println(o)
+}
+
+func test() {
     var a int
     var b float32
     var c bool
