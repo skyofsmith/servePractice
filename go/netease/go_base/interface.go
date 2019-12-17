34b4ad52e3d66206633c53732b28ae55cf14c584
skyofsmith <skyofsmith@163.com>
2019-12-17 22:43:28 +0800

    interface

---
+++ go/netease/go_base/interface.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type USB interface {
+	Name() string
+	Connecter
+}
+
+type Connecter interface {
+	Connect()
+}
+
+type PhoneConnecter struct {
+	name string
+}
+
+func (pc PhoneConnecter) Name() string {
+	return pc.name
+}
+
+func (pc PhoneConnecter) Connect() {
+	fmt.Println("Connect:", pc.name)
+}
+
+func main() {
+	//var a USB
+	a := PhoneConnecter{"PhoneConnecter"}
+	a.Connect()
+	DisConnect(a)
+}
+
+func DisConnect(usb UBS) {
+	if pc, ok := usb.PhoneConnecter; ok {
+		fmt.Println("Disconnected.", pc.name)
+		return
+	}
+	fmt.Println("Unknown device.")
+}
