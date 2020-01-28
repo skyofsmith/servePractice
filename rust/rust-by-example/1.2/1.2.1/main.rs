8f3d13b70cc82fa0812f23643e2b7a0b13ba1480
skyofsmith <skyofsmith@163.com>
2020-01-28 23:07:02 +0800

    1.2.1

---
+++ rust/rust-by-example/1.2/1.2.1/main.rs
@@ -0,0 +1,10 @@
+
+#![allow(unused_variables)]
+fn main() {
+// 这个结构体不能使用 `fmt::Display` 或 `fmt::Debug` 来进行打印。
+struct UnPrintable(i32);
+
+// `derive` 属性会自动创建所需的实现，使这个 `struct` 能使用 `fmt::Debug` 打印。
+#[derive(Debug)]
+struct DebugPrintable(i32);
+}
