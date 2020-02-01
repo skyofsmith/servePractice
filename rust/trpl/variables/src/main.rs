d7ccd62656816096b0667ceb6aa21b7c2513223b
skyofsmith <skyofsmith@163.com>
2020-02-01 22:24:36 +0800

    variables

---
+++ rust/trpl/variables/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const MAX_POINTS: u32 = 100_000;
+    println!("MAX_POINTS is: {}", MAX_POINTS);
+
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    // warn
+    // ^^^^^^ help: consider prefixing with an underscore: `_spaces`
+
+//    let mut spaces = "   ";
+//    spaces = spaces.len();
+    // error
+}
