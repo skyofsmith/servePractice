3b54c30d66c9540d904728eb51d55a17722ed1dc
skyofsmith <skyofsmith@163.com>
2020-02-22 23:54:05 +0800

    associated functions

--- rust/trpl/method_syntax/src/main.rs
+++ rust/trpl/method_syntax/src/main.rs
@@ -8,6 +8,14 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
@@ -16,5 +24,19 @@ fn main() {
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
+    );
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let rect4 = Rectangle::square(3);
+    println!(
+        "The area is {:#?}, the rectangle is {} square pixels.",
+        rect4,
+        rect4.area()
     );
 }
