6583d1be254513da22753e6462c5d8965ec5c3d1
skyofsmith <skyofsmith@163.com>
2020-02-26 12:21:57 +0800

    the-match

---
+++ rust/trpl/the-match/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
