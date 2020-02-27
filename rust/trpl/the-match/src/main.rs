05ce90ae3686795039edef0babbb0ea4448dddd3
skyofsmith <skyofsmith@163.com>
2020-02-27 13:09:51 +0800

    Option<T>

--- rust/trpl/the-match/src/main.rs
+++ rust/trpl/the-match/src/main.rs
@@ -1,21 +1,39 @@
 fn main() {
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+    
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("five is {:#?}, six is {:#?}, none is {:#?}", five, six, none);
 }
 
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
+}
+
+#[derive(Debug)] // 这样可以可以立刻看到州的名称
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        },
+        Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
