2d1a887d8b9d435e3e5d3bdd08ec887fdd9003b1
skyofsmith <skyofsmith@163.com>
2020-03-08 23:18:44 +0800

    use dependencies

--- rust/trpl/defining_modules_to_control_scope_and_privacy/src/main.rs
+++ rust/trpl/defining_modules_to_control_scope_and_privacy/src/main.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
 use std::fmt::Result;
 use std::io::Result as IoResult;
+use rand::Rng;
 
 fn main() {
     let mut map = HashMap::new();
     map.insert(1, 2);
-    println!("map is {:#?}", map)
+    println!("map is {:#?}", map);
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("secret_number is {}", secret_number);
     
 }
