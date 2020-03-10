7be50cacbd2aff0fa7abdd721ee85066e5381111
skyofsmith <skyofsmith@163.com>
2020-03-10 13:41:27 +0800

    nested path

--- rust/trpl/defining_modules_to_control_scope_and_privacy/src/lib2.rs
+++ rust/trpl/defining_modules_to_control_scope_and_privacy/src/lib2.rs
@@ -1,8 +1,12 @@
 use std::fmt::Result;
 use std::io::Result as IoResult;
-use std::{cmp::Ordering, io};
 // use std::cmp::Ordering;
 // use std::io;
+use std::{cmp::Ordering, io};
+
+// use std::io;
+// use std::io::Write;
+use std::io::{self, Write};
 
 fn function1() -> Result {
     // --snip--
