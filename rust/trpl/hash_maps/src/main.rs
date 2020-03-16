cce42fd564ca65b7d36243a2d879325688cdd9c8
skyofsmith <skyofsmith@163.com>
2020-03-16 22:58:44 +0800

    hash_map

---
+++ rust/trpl/hash_maps/src/main.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+
+fn main() {
+    test_new_hash_map();
+    test_auth();
+}
+
+fn test_new_hash_map() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    println!("scores is {:#?}", scores);
+
+    let teams  = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("scores is {:#?}", scores);
+}
+
+fn test_auth() {
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+//    println!("field_name is {}, field_value is {}", field_name, field_value);
+    println!("map is {:#?}", map);
+}
