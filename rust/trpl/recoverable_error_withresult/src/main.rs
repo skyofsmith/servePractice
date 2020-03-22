12e51a6107870c6b402d2f9c09c1c61b150a153c
skyofsmith <skyofsmith@163.com>
2020-03-22 22:45:48 +0800

    propagating

--- rust/trpl/recoverable_error_withresult/src/main.rs
+++ rust/trpl/recoverable_error_withresult/src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::fs::File;
 use std::io::ErrorKind;
 
@@ -18,4 +19,22 @@ fn main() {
     let f = File::open("hello.txt").expect("Failed to open hello.txt");
 
     println!("f is {:#?}", f);
+}
+
+
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
