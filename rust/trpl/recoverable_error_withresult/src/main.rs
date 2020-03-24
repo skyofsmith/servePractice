b83274f144454ade763af37b6a5c24018895dba4
skyofsmith <skyofsmith@163.com>
2020-03-24 13:03:57 +0800

    ? operator

--- rust/trpl/recoverable_error_withresult/src/main.rs
+++ rust/trpl/recoverable_error_withresult/src/main.rs
@@ -46,3 +46,12 @@ fn read_username_from_file2() -> Result<String, io::Error> {
     f.read_to_string(&mut s)?;
     Ok(s)
 }
+
+fn read_username_from_file3() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
