9afb828de8256d000b4e1f0814c514d401504119
skyofsmith <skyofsmith@163.com>
2020-03-30 13:17:00 +0800

    generics

---
+++ rust/trpl/generics/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    test_find_max_value();
+    
+}
+
+fn test_find_max_value() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+}
