fee9f88208e117616d96e8f120987833ba1c053d
skyofsmith <skyofsmith@163.com>
2020-03-31 13:04:52 +0800

    find 2 max

--- rust/trpl/generics/src/main.rs
+++ rust/trpl/generics/src/main.rs
@@ -1,10 +1,37 @@
 fn main() {
     test_find_max_value();
-    
+    test_find_two_max_value();
 }
 
 fn test_find_max_value() {
     let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+}
+
+
+fn test_find_two_max_value() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
     let mut largest = number_list[0];
 
