2f6e3e82c72a0e42cb900a468db6487375ca626a
skyofsmith <skyofsmith@163.com>
2020-04-01 12:45:33 +0800

    find max by function

--- rust/trpl/generics/src/main.rs
+++ rust/trpl/generics/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     test_find_max_value();
     test_find_two_max_value();
+    test_find_max_by_function();
 }
 
 fn test_find_max_value() {
@@ -42,4 +43,28 @@ fn test_find_two_max_value() {
     }
 
     println!("The largest number is {}", largest);
+}
+
+fn test_find_max_by_function() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+}
+
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
 }
