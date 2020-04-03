a9a74ffcc4db22da50a1359d7ac76fc9494de780
skyofsmith <skyofsmith@163.com>
2020-04-03 13:04:56 +0800

    struct syntax

--- rust/trpl/syntax/src/main.rs
+++ rust/trpl/syntax/src/main.rs
@@ -1,13 +1,6 @@
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest_i32(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest_char(&char_list);
-    println!("The largest char is {}", result);
+    test_func();
+    test_struct();
 }
 
 fn largest_i32(list: &[i32]) -> i32 {
@@ -32,4 +25,28 @@ fn largest_char(list: &[char]) -> char {
     }
 
     largest
+}
+
+fn test_func() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+}
+
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn test_struct() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+    println!("integer is {:#?}, float is {:#?}", integer, float);
 }
