fb7d29867f5f4606571141bc293ccff47f3dda12
skyofsmith <skyofsmith@163.com>
2020-04-04 12:31:17 +0800

    method syntax

--- rust/trpl/syntax/src/main.rs
+++ rust/trpl/syntax/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     test_func();
     test_struct();
+    test_method();
 }
 
 fn largest_i32(list: &[i32]) -> i32 {
@@ -49,4 +50,28 @@ fn test_struct() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
     println!("integer is {:#?}, float is {:#?}", integer, float);
+}
+
+// #[derive(Debug)]
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
+
+// #[derive(Debug)]
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn test_method() {
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
 }
