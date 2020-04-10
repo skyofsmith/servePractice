edb6f976166af86082827ecd48a836847fdf11d8
skyofsmith <skyofsmith@163.com>
2020-04-10 23:32:06 +0800

    lib

--- rust/trpl/traits/src/lib.rs
+++ rust/trpl/traits/src/lib.rs
@@ -31,3 +31,7 @@ impl Summary for Tweet {
 pub fn notify(item: impl Summary) {
     println!("Breaking news! {}", item.summarize());
 }
+
+pub fn notify<T: Summary>(item: T) {
+    println!("Breaking news! {}", item.summarize());
+}
