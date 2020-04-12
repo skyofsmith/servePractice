92ae2014c21a1f27edd4c484ec37b22f1cdcc2ac
skyofsmith <skyofsmith@163.com>
2020-04-12 23:06:06 +0800

    variable

--- rust/base/variable/src/main.rs
+++ rust/base/variable/src/main.rs
@@ -1,3 +1,5 @@
+const MAX_POINT: u32 = 100000;
+
 fn main() {
     //1、变量定义
     let a = 1;
@@ -7,4 +9,12 @@ fn main() {
     println!("b = {}", b);
     b = 2;
     println!("b = {}", b);
+
+    //2、隐藏
+    let b: f32 = 1.1;
+    println!("b = {}", b);
+
+    //3、常量
+    println!("MAX_POINT = {}", MAX_POINT);
+
 }
