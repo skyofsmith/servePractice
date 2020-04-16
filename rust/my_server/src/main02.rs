57fd6efad33c4e20cc885cc46c6b803be724bc26
skyofsmith <skyofsmith@163.com>
2020-04-16 22:21:54 +0800

    response a html

--- rust/my_server/src/main02.rs
+++ rust/my_server/src/main02.rs
@@ -1,17 +1,10 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
-use std::fs;
+ use std::net::{TcpListener, TcpStream};
+use std::io::Read;
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-
-    let content = fs::read_to_string("main.html").unwrap();
-//    let response = "HTTP/1.1 200 OK\r\n\r\n{}";
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", content);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
 }
 
 fn main() -> std::io::Result<()> {
