f40716ad6e3fbb7f9a482edede35e6cdd4918022
skyofsmith <skyofsmith@163.com>
2020-04-21 22:54:57 +0800

    option1

---
+++ rust/base/option1/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let some_number = Some(5);
+    let some_string = Some(String::from("a string"));
+    let absent_number: Option<i32> = None;
+    println!("some_number = {:?}", some_number);
+    println!("some_string = {:?}", some_string);
+    println!("absent_number = {:?}", absent_number);
+
+    let x: i32 = 5;
+    let y: Option<i32> = Some(5);
+    let mut temp = 0;
+    match y {
+        Some(i) => { temp = i; }
+        None => {
+            println!("do nothing");
+        }
+    }
+    // let sum = x + y; //  error
+    let sum = x + temp;
+    println!("sum = {}", sum);
+
+    let result = plus_one(y);
+    match result {
+        Some(i) => println!("result = {}", i),
+        None => println!("nothing"),
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(x) => Some(x+1),
+    }
+}
