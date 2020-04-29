c7189be90cc0e887b3190026492ac9472188746b
skyofsmith <skyofsmith@163.com>
2020-04-29 22:44:19 +0800

    lib

--- rust/base/crates/src/main.rs
+++ rust/base/crates/src/main.rs
@@ -1,10 +1,10 @@
-use mylib::factory::produce_refrigerator;
+// use mylib::factory::produce_refrigerator;
 // use mylib::factory::produce_refrigerator::produce_re; // 不推荐使用
-use mylib::factory::produce_washing_machine as A;
+// use mylib::factory::produce_washing_machine as A;
 
-// use mylib::factory::*; // 导入所有包
+use mylib::factory::*; // 导入所有包
 
-mod modA {
+mod mod_a {
     #[derive(Debug)]
     pub struct A {
         pub number: i32,
@@ -21,17 +21,41 @@ mod modA {
             println!("number: {}, name: {}", self.number, self.name)
         }
     }
+
+    pub mod mod_b {
+        pub fn print_b() {
+            println!("B");
+        }
+
+        pub mod mod_c {
+            pub fn print_c() {
+                println!("C");
+                super::print_b();
+            }
+        }
+    }
+
 }
+
+use mod_a::A;
 
 fn main() {
     // 绝对路径
     mylib::factory::produce_refrigerator::produce_re();
 
     produce_refrigerator::produce_re();
-    A::produce_re();
+    produce_washing_machine::produce_re();
 
     // produce_re();  // 不推荐使用
 
-    let a = modA::A::new_a();
+    let a = A::new_a();
     a.print_a();
+
+    let number = a.number;
+    println!("number = {}", number);
+
+    // let name = a.name; // private field
+    // println!("name = {}", name);
+
+    mod_a::mod_b::mod_c::print_c();
 }
