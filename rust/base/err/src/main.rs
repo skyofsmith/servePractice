a0596bf1e0c40ca6d1ff60f38655d59f087228bc
skyofsmith <skyofsmith@163.com>
2020-05-01 23:32:22 +0800

    panic to catch error finished

--- rust/base/err/src/main.rs
+++ rust/base/err/src/main.rs
@@ -16,11 +16,13 @@
 //2、传播错误的简写方式，提倡的方式
 //3、更进一步的简写
 //4、什么时候用panic!，什么时候用Result
+//(1)示例、代码原型、测试用panic!\unwrap\expect
+//(2)实际项目中应该用Result
 //5、Option和Result
 use std::fs::File;
 use std::io;
 use std::io::Read;
-
+/* 
 fn read_username_from_file() -> Result<String, io::Error> {
     let f = File::open("hello.txt");
     let mut f = match f {
@@ -33,6 +35,21 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Ok(_) => Ok(s),
         Err(error) => Err(error),
     }
+}
+ */
+/* 
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+ */
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
 }
 
 fn main() {
