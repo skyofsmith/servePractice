325a3c35ad510d7997de27574e885e1a31f6b17c
skyofsmith <skyofsmith@163.com>
2020-05-16 19:33:05 +0800

    deref polymorphic

--- rust/advanced/deref/src/main.rs
+++ rust/advanced/deref/src/main.rs
@@ -3,6 +3,21 @@
 // let b = &a;
 // let c = *a; // 解引用
 
+use std::ops::Deref;
+struct MyBox<T>(T);
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
 fn main() {
     {
         let x = 5;
@@ -15,24 +30,27 @@ fn main() {
     }
 
     {
-        use std::ops::Deref;
-        struct MyBox<T>(T);
-        impl<T> MyBox<T> {
-            fn new(x: T) -> MyBox<T> {
-                MyBox(x)
-            }
-        }
-
-        impl<T> Deref for MyBox<T> {
-            type Target = T;
-            fn deref(&self) -> &T {
-                &self.0
-            }
-        }
-
         let x = 5;
         let y = MyBox::new(x);
         assert_eq!(5, x);
         assert_eq!(5, *y);
     }
+
+    {
+        let m = MyBox::new(String::from("Rust"));
+        hello(&m);  //将MyBox变为&String，再将String的解引用变为字符串slice。   &str
+        hello2(&m);
+    }
 }
+
+fn hello(name: &str) {
+    println!("Hello, {}", name);
+}
+
+fn hello2(name: &String) {
+    println!("Hello, {}", name);
+}
+//解引用多态与可变性交互：
+//（1）当T：Deref<Target=U>时，从&T到&U
+//（2）当T：DerefMut<Target=U>时，从&mut T 到&mut U
+//（3）当T：Deref<Target=U>时，从&mut T到&U
