c159a004b90088fb43750414e061c09ef81fa58b
skyofsmith <skyofsmith@163.com>
2020-05-17 13:42:20 +0800

    rc count

--- rust/advanced/rc/src/main.rs
+++ rust/advanced/rc/src/main.rs
@@ -2,8 +2,10 @@
 //     Cons(i32, Box<List>),
 //     Nil,
 // }
+//通过Rc<T>允许程序的多个部分之间只读的共享数据，因为相同位置的多个可变引用可能会造成数据竞争和不一致。
 use std::rc::Rc;
 
+#[derive(Debug)]
 enum List {
     Cons(i32, Rc<List>),
     Nil
@@ -19,5 +21,20 @@ fn main() {
         let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
         let b = Cons(3, Rc::clone(&a));
         let c = Cons(3, a.clone());
+        println!("b = {:#?}, c = {:#?}", b, c);
+    }
+    {
+        let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+        println!("count after creating a = {}", Rc::strong_count(&a));
+        
+        let b = Cons(3, Rc::clone(&a));
+        println!("count after bind to b = {}", Rc::strong_count(&a));
+
+        {
+            let c = Cons(3, a.clone());
+            println!("count after bind to c = {}", Rc::strong_count(&a));
+        }
+
+        println!("count at end, a = {}", Rc::strong_count(&a));
     }
 }
