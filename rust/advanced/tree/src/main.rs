bcbfc24a9da89484e2da414669607df954272c36
skyofsmith <skyofsmith@163.com>
2020-05-21 21:12:41 +0800

    tree

--- rust/advanced/tree/src/main.rs
+++ rust/advanced/tree/src/main.rs
@@ -10,30 +10,64 @@ struct Node {
 
 // use crates::Node;
 fn main() {
-    let leaf = Rc::new(
-        Node {
-            value: 3,
-            parent: RefCell::new(
-                Weak::new()
-            ),
-            children: RefCell::new(
-                vec![]
-            )
-        }
-    );
-    println!("leaf parent = {:#?}", leaf.parent.borrow().upgrade());
+    {
+        let leaf = Rc::new(
+            Node {
+                value: 3,
+                parent: RefCell::new(
+                    Weak::new()
+                ),
+                children: RefCell::new(
+                    vec![]
+                )
+            }
+        );
+        println!("leaf parent = {:#?}", leaf.parent.borrow().upgrade());
 
-    let branch =  Rc::new(
-        Node {
-            value: 5,
-            parent: RefCell::new(
-                Weak::new()
-            ),
-            children: RefCell::new(
-                vec![Rc::clone(&leaf)]
-            )
+        let branch =  Rc::new(
+            Node {
+                value: 5,
+                parent: RefCell::new(
+                    Weak::new()
+                ),
+                children: RefCell::new(
+                    vec![Rc::clone(&leaf)]
+                )
+            }
+        );
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        println!("leaf parent = {:#?}", leaf.parent.borrow().upgrade());
+    }
+    {
+        let leaf = Rc::new(
+            Node{
+                value: 3,
+                parent: RefCell::new(
+                    Weak::new()
+                ),
+                children: RefCell::new(vec![]),
+            }
+        );
+        println!("1 leaf strong = {}, weak = {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+        
+        {
+            let branch = Rc::new(
+                Node{
+                    value: 5,
+                    parent: RefCell::new(
+                        Weak::new()
+                    ),
+                    children: RefCell::new(vec![Rc::clone(&leaf)]),
+                }
+            );
+            println!("1 branch strong = {}, weak = {}", Rc::strong_count(&branch), Rc::weak_count(&branch));
+
+            *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+            
+            println!("2 branch strong = {}, weak = {}", Rc::strong_count(&branch), Rc::weak_count(&branch));
+            println!("2 leaf strong = {}, weak = {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
         }
-    );
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
-    println!("leaf parent = {:#?}", leaf.parent.borrow().upgrade());
+        println!("3 leaf strong = {}, weak = {}", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+    }
 }
+
