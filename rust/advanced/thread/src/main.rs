1b1e7e7f540f8f58fb1d9831d860db50495e4383
skyofsmith <skyofsmith@163.com>
2020-05-21 23:10:34 +0800

    thread handle join

--- rust/advanced/thread/src/main.rs
+++ rust/advanced/thread/src/main.rs
@@ -1,16 +1,41 @@
 use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let v = vec![1, 2, 3];
+    {
+        let v = vec![1, 2, 3];
 
-    let handle = thread::spawn(move || {
-        // sleep(10);
-        println!("v: {:?}", v);
-    });
+        let handle = thread::spawn(move || {
+            // sleep(10);
+            println!("v: {:?}", v);
+        });
 
-    // drop(v);
-    // println!("v: {:?}", v);  // error v moved
-    
+        // drop(v);
+        // println!("v: {:?}", v);  // error v moved
+        
 
-    handle.join().unwrap();
+        handle.join().unwrap();
+    }
+    {
+        let handle = thread::spawn(|| {
+            for i in 1..10 {
+                println!("number {} in spawn thread!", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        for i in 1..5 {
+            println!("number {} in main thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+
+        handle.join().unwrap();
+
+        println!("++++++++++++++++");
+        for i in 1..5 {
+            println!("number {} in main thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+
+    }
 }
