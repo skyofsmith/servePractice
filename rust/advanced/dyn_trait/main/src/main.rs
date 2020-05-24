cdf4ac66eccacee50e7617724606cd94a55c035e
skyofsmith <skyofsmith@163.com>
2020-05-24 23:04:53 +0800

    dyn_trait

---
+++ rust/advanced/dyn_trait/main/src/main.rs
@@ -0,0 +1,23 @@
+use gui::{Screen, Button, SelectBox};
+
+fn main() {
+    let s = Screen {
+        components: vec![
+            Box::new(Button{
+                width: 50,
+                height: 10,
+                label: String::from("ok"),
+            }),
+            Box::new(SelectBox{
+                width: 60,
+                height: 40,
+                option: vec![
+                    String::from("Yes"),
+                    String::from("No"),
+                    String::from("MayBe"),
+                ],
+            })
+        ],
+    };
+    s.run();
+}
