d504e6fe56d8337cd9fcb3fca8bd83cc82f0ea8b
skyofsmith <skyofsmith@163.com>
2020-05-27 21:56:42 +0800

    all pattern

--- rust/advanced/all_pattern/src/main.rs
+++ rust/advanced/all_pattern/src/main.rs
@@ -44,4 +44,145 @@ fn main() {
             _ => println!("other"),
         };
     }
+
+    //解构并分解值
+    //解构元祖、结构体、枚举、引用
+    {
+        //解构结构体
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+
+        let p = Point { x: 1, y: 2};
+        let Point { x: a, y: b } = p;
+        assert_eq!(1, a);
+        assert_eq!(2, b);
+
+        let Point { x, y } = p;
+        assert_eq!(1, x);
+        assert_eq!(2, y);
+
+        let p = Point { x: 1, y: 0};
+        match p {
+            Point { x, y: 0 } => println!("x axis, x = {}", x),
+            Point { x: 0, y } => println!("y axis, y = {}", y),
+            Point { x, y } => println!("other, x = {}, y = {}", x, y),
+        };
+    }
+    {
+        //结构枚举
+        enum Message {
+            Quit,
+            Move { x: i32, y: i32 },
+            Write(String),
+            ChangeColor(i32, i32, i32),
+        }
+
+        let msg = Message::ChangeColor(0, 160, 255);
+        match msg {
+            Message::Quit => {
+                println!("quit");
+            },
+            Message::Move { x, y } => {
+                println!("move, x: {}, y: {}", x, y);
+            },
+            Message::Write(text) => println!("write msg: {}", text),
+            Message::ChangeColor(r, g, b) => {
+                println!("color, r: {}, g: {}, b: {}", r, g, b);
+            },
+        }
+    }
+    {
+        //结构嵌套枚举
+        enum Color {
+            Rgb(i32, i32, i32),
+            Hsv(i32, i32, i32),
+        }
+        enum Message {
+            Quit,
+            Move { x: i32, y: i32 },
+            Write(String),
+            ChangeColor(Color),
+        }
+
+        let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+        match msg {
+            Message::ChangeColor(Color::Rgb(r, g, b)) => {
+                println!("rgb color, r: {}, g: {}, b: {}", r, g, b);
+            },
+            Message::ChangeColor(Color::Hsv(h, s, v)) => {
+                println!("hsv color, h: {}, s: {}, v: {}", h, s, v);
+            },
+            _ => ()
+        }
+    }
+    {
+        //解构元祖
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+
+        let ((a, b), Point{ x, y }) = ((1, 2), Point{ x: 3, y: 4});
+        println!("a: {}, b: {}, x: {}, y: {}", a, b, x, y);
+    }
+
+    //忽略模式中的值
+    {
+        fn foo(_: i32, y: i32) {
+            println!("y = {}", y);
+        }
+
+        trait A {
+            fn bar(x: i32, y: i32);
+        }
+
+        struct B  {}
+        impl A for B {
+            fn bar(_: i32, y: i32) {
+                println!("y = {}", y);
+            }
+        }
+        foo(1, 2);
+
+        let numbers = (1, 2, 3, 4);
+        match numbers {
+            (one, _, three, _) => {
+                println!("one: {}, three: {}", one, three);
+            },
+        }
+    }
+    {
+        let _x = 5;
+        let _y = 5;
+        println!("_x: {}", _x);
+
+        let s = Some(String::from("hello"));
+        if let Some(_c) = s {
+            println!("found a string");
+        }
+        // println!("s: {:?}", s); // error
+
+
+        let s = Some(String::from("hello"));
+        if let Some(_) = s {
+            println!("found a string");
+        }
+        println!("s: {:?}", s); // no error
+    }
+
+    {
+        let numbers = (1,2,3,4,5,6,7);
+        match numbers {
+            (first, .., last) => {
+                println!("first: {}, last: {}", first, last);
+            },
+        };
+        // match numbers {
+        //     (.., second, ..) => { // error occur
+        //         println!("second: {}", second);
+        //     },
+        // };
+    }
 }
