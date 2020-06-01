ad2b92f6505c216de1d05658061281c3755069b4
skyofsmith <skyofsmith@163.com>
2020-06-01 22:50:16 +0800

    father trait; newtype

--- rust/advanced/htrait/src/main.rs
+++ rust/advanced/htrait/src/main.rs
@@ -151,5 +151,50 @@ fn main() {
         println!("baby_name: {}", Dog::baby_name());
         // println!("baby_name: {}", Animal::baby_name());  // error
         println!("baby_name: {}", <Dog as Animal>::baby_name());
+
+        //完全限定语法定义：
+        //<Type as Trait>::function(.....)
+    }
+
+    {
+        //4、父 trait 用于在另一个 trait 中使用某 trait 的功能
+        //有时我们可能会需要某个 trait 使用另一个 trait 的功能。
+        //在这种情况下，需要能够依赖相关的 trait 也被实现。
+        //这个所需的 trait 是我们实现的 trait 的 父（超） trait（supertrait）。
+
+        use std::fmt;
+        trait OutPrint: fmt::Display { //要求实现Display trait
+            fn out_print(&self) {
+                let output = self.to_string();
+                println!("output: {}", output);
+            }
+        }
+
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+
+        impl OutPrint for Point {}
+
+        impl fmt::Display for Point {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "({}, {})", self.x, self.y)
+            }
+        }
+
+    }
+
+    {
+        use std::fmt;
+        struct Wrapper(Vec<String>);
+        impl fmt::Display for Wrapper {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "({})", self.0.join(","))
+            }
+        }
+
+        let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+        println!("w = {}", w);
     }
 }
