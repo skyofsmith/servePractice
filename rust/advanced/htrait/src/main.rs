c8c6880d80b74759ea638e231ff6651a7474e1b9
skyofsmith <skyofsmith@163.com>
2020-06-01 22:34:52 +0800

    trait method

--- rust/advanced/htrait/src/main.rs
+++ rust/advanced/htrait/src/main.rs
@@ -95,4 +95,61 @@ fn main() {
         //    fn add(self, rhs: RHS) -> Self::Output;
         //}
     }
+
+    {
+        trait A {
+            fn print(&self);
+        }
+        
+        trait B {
+            fn print(&self);
+        }
+        
+        struct MyType;
+
+        impl A for MyType {
+            fn print(&self) {
+                println!("A trait for MyType");
+            }
+        }
+
+        impl MyType {
+            fn print(&self) {
+                println!("MyType");
+            }
+        }
+
+        impl B for MyType {
+            fn print(&self) {
+                println!("B trait for MyType");
+            }
+        }
+
+        let my_type = MyType;
+        my_type.print(); //等价于 MyType::print(&my_type);
+        A::print(&my_type);
+        B::print(&my_type);
+
+        trait Animal {
+            fn baby_name() -> String;
+        }
+
+        struct Dog;
+
+        impl Dog {
+            fn baby_name() -> String {
+                String::from("Spot")
+            }
+        }
+
+        impl Animal for Dog {
+            fn baby_name() -> String {
+                String::from("puppy")
+            }
+        }
+
+        println!("baby_name: {}", Dog::baby_name());
+        // println!("baby_name: {}", Animal::baby_name());  // error
+        println!("baby_name: {}", <Dog as Animal>::baby_name());
+    }
 }
